f1c9ccc091c530ce5baa3a16fdbe5ab43607bcee
hamidbinameziane <104109540+hamidbinameziane@users.noreply.github.com>
2023-10-31 20:14:05 +0100

    3-adding home.html

--- api/main.py
+++ api/main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hamidbinameziane'
+
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
