4dc9369b279dbd8cfffba34612297bd6865c3143
hamidbinameziane <104109540+hamidbinameziane@users.noreply.github.com>
2023-11-01 20:17:24 +0100

    9-modifing touch events

--- api/static/js/can2.js
+++ api/static/js/can2.js
@@ -149,53 +149,16 @@ window.addEventListener('load' , ()=> {
 
 
 
-    function touchHandler(event)
-    {
-        var touches = event.changedTouches,
-            first = touches[0],
-            type = "";
-        switch(event.type)
-        {
-            case "touchstart": type = "mousedown"; break;
-            case "touchmove":  type = "mousemove"; break;        
-            case "touchend":   type = "mouseup";   break;
-            default:           return;
-        }
     
-        // initMouseEvent(type, canBubble, cancelable, view, clickCount, 
-        //                screenX, screenY, clientX, clientY, ctrlKey, 
-        //                altKey, shiftKey, metaKey, button, relatedTarget);
-    
-        var simulatedEvent = document.createEvent("MouseEvent");
-        simulatedEvent.initMouseEvent(type, true, true, window, 1, 
-                                      first.screenX, first.screenY, 
-                                      first.offsetX, first.offsetY, false, 
-                                      false, false, false, 0/*left*/, null);
-    
-        first.target.dispatchEvent(simulatedEvent);
-        event.preventDefault();
-    }
-    
-    function init() 
-    {
-        document.addEventListener("touchstart", touchHandler, true);
-        document.addEventListener("touchmove", touchHandler, true);
-        document.addEventListener("touchend", touchHandler, true);
-        document.addEventListener("touchcancel", touchHandler, true);    
-    }
 
-    canvas.addEventListener('mousedown', startP)
-    canvas.addEventListener('mouseup', endP)
-    canvas.addEventListener('mousemove', Draw)
     canvas.addEventListener('pointerdown', startP)
     canvas.addEventListener('pointerup', endP)
     canvas.addEventListener('pointerout', endP)
     canvas.addEventListener('pointerout', endP)
     canvas.addEventListener('pointermove', Draw)
-    canvas.addEventListener("touchstart", init);
-    canvas.addEventListener("touchend", init);
-    canvas.addEventListener("touchcancel", init);
-    canvas.addEventListener("touchmove", init);
+    canvas.addEventListener("touchmousedown", startP);
+    canvas.addEventListener("touchmouseup", endP);
+    canvas.addEventListener("touchmousemove", Draw);
     clr.addEventListener('input', clrC)
     sze.addEventListener('input', szeC)
     num.addEventListener('input', numC)
